1c16718212c8a0674262803be01b787ce1b350de
juanmondra <tiertrainingacademy@gmail.com>
2021-11-16 22:02:38 -0600

    W databases

---
+++ messages.js
@@ -0,0 +1,13 @@
+// string database
+const subjects = ["You" , "Mary" , "John" , "Jacob" , "Chris"];
+const verbs = ['marry' , 'kill' , 'eat', 'grab' , 'cuddle with'];
+const extra = ['a mouse' , 'a tree' , 'forever' , 'me' , 'a butthole'] 
+
+const generateRandomNumber = () => {
+    let num = math.floor(math.random) * 6;
+    return num;
+};
+
+const subjectRand = () => {
+
+};

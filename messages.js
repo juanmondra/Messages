5d5a22461b4428ef9c919eac53b19d1f290240ca
juanmondra <tiertrainingacademy@gmail.com>
2021-11-16 22:22:37 -0600

    Messages

--- messages.js
+++ messages.js
@@ -3,11 +3,30 @@ const subjects = ["You" , "Mary" , "John" , "Jacob" , "Chris"];
 const verbs = ['marry' , 'kill' , 'eat', 'grab' , 'cuddle with'];
 const extra = ['a mouse' , 'a tree' , 'forever' , 'me' , 'a butthole'] 
 
-const generateRandomNumber = () => {
-    let num = math.floor(math.random) * 6;
-    return num;
-};
 
 const subjectRand = () => {
+    let num = Math.floor(Math.random() * 5);
+    return subjects[num];
 
-};
+}
+
+const verbRand = () => {
+    let num1 = Math.floor(Math.random() * 5);
+    return verbs[num1];
+
+}
+
+const extraRand = () => {
+    let num2 = Math.floor(Math.random() * 5);
+    return extra[num2];
+
+}
+
+
+
+
+const generateMessage = () => {
+    console.log(`Your message is: ${subjectRand()} ${verbRand()} ${extraRand()}`);
+}
+
+generateMessage();
